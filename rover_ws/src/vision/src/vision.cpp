785289ae970564bd848cd0fcc2dd5f162f1cec52
Gary Ellingson <gellings13@gmail.com>
2015-12-08 11:57:18 -0700

    vision now has the form of a hardware driver for the ros image_pipeline, not complete but I need a place to store it

--- rover_ws/src/vision/src/vision.cpp
+++ rover_ws/src/vision/src/vision.cpp
@@ -7,6 +7,7 @@
 
 #include "vision.h"
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
 
 namespace vision
 {
@@ -17,12 +18,34 @@ Vision::Vision():
 {
     nh_private_.param<int>("camera1", device1, 0);
     nh_private_.param<int>("camera2", device2, 1);
+    nh_private_.param<bool>("useStereo", use_stereo_, true);
+    nh_private_.param<std::string>("file", file_path_, "~/rostesting/rover_ws/src/vision/config");
 
-    if(!v1.open(device1) )// || !v2.open(device2))
-        ROS_ERROR("could not open one or both cameras");
+    if(use_stereo_)
+    {
+        if(!v1.open(device1) || !v2.open(device2))
+            ROS_ERROR("could not open one or both cameras");
 
-    image_publisher_ = nh_.advertise<sensor_msgs::Image>("image", 1);
+        image_publisher_ = nh_.advertise<sensor_msgs::Image>("right/image", 1);
+        camera_publisher_ = nh_.advertise<sensor_msgs::CameraInfo>("right_camera", 1);
 
+        srv_request_set_cam_info_ = nh_.advertiseService("right_camera/set_camera_info", &Vision::requestSetCamInfoService, this);
+
+        left_image_publisher_ = nh_.advertise<sensor_msgs::Image>("left/image", 1);
+        left_camera_publisher_ = nh_.advertise<sensor_msgs::CameraInfo>("left_camera", 1);
+
+        srv_request_set_left_cam_info_ = nh_.advertiseService("left_camera/set_camera_info", &Vision::requestSetLeftCamInfoService, this);
+    }
+    else
+    {
+        if(!v1.open(device1))
+            ROS_ERROR("could not open the camera");
+
+        image_publisher_ = nh_.advertise<sensor_msgs::Image>("image", 1);
+        camera_publisher_ = nh_.advertise<sensor_msgs::CameraInfo>("camera", 1);
+
+        srv_request_set_cam_info_ = nh_.advertiseService("camera/set_camera_info", &Vision::requestSetCamInfoService, this);
+    }
 }
 
 Vision::~Vision()
@@ -51,6 +74,55 @@ void Vision::publish_image()
         cv_img.encoding = sensor_msgs::image_encodings::BGR8;
         image_publisher_.publish(cv_img.toImageMsg());
     }
+
+    sensor_msgs::CameraInfo cam;
+    camera_publisher_.publish(cam);
+}
+
+//void Vision::publish_camera()
+//{
+//    sensor_msgs::CameraInfo cam;
+//    camera_publisher_.publish(cam);
+
+//    if(use_stereo_)
+//    {
+//        sensor_msgs::CameraInfo lcam;
+//        left_camera_publisher_.publish(lcam);
+//    }
+//}
+
+bool Vision::requestSetCamInfoService(sensor_msgs::SetCameraInfo::Request &req, sensor_msgs::SetCameraInfo::Response &res)
+{
+    std::stringstream s;
+    s << file_path_ << "/cameraParam.yaml";
+    cv::FileStorage fs(s.str(), cv::FileStorage::WRITE);
+    fs << "height" << (int)req.camera_info.height << "width" << (int)req.camera_info.width;
+    fs << "distortion_model" << req.camera_info.distortion_model << "D" << req.camera_info.D;
+    fs << "camMatrix" << cv::Mat(3,3,CV_64FC1,&req.camera_info.K);
+    fs << "sterRect" << cv::Mat(3,3,CV_64FC1,&req.camera_info.R);
+    fs << "project" << cv::Mat(3,4,CV_64FC1,&req.camera_info.P);
+    fs << "binning" << (int)req.camera_info.binning_x << (int)req.camera_info.binning_y;
+    fs << "roi" << (int)req.camera_info.roi.height << (int)req.camera_info.roi.width;
+    fs.release();
+    res.success = true;
+    return true;
+}
+
+bool Vision::requestSetLeftCamInfoService(sensor_msgs::SetCameraInfo::Request &req, sensor_msgs::SetCameraInfo::Response &res)
+{
+    std::stringstream s;
+    s << file_path_ << "/leftCameraParam.yaml";
+    cv::FileStorage fs(s.str(), cv::FileStorage::WRITE);
+    fs << "height" << (int)req.camera_info.height << "width" << (int)req.camera_info.width;
+    fs << "distortion_model" << req.camera_info.distortion_model << "D" << req.camera_info.D;
+    fs << "camMatrix" << cv::Mat(3,3,CV_64FC1,&req.camera_info.K);
+    fs << "sterRect" << cv::Mat(3,3,CV_64FC1,&req.camera_info.R);
+    fs << "project" << cv::Mat(3,4,CV_64FC1,&req.camera_info.P);
+    fs << "binning" << (int)req.camera_info.binning_x << (int)req.camera_info.binning_y;
+    fs << "roi" << (int)req.camera_info.roi.height << (int)req.camera_info.roi.width;
+    fs.release();
+    res.success = true;
+    return true;
 }
 
 } //namespace vision
